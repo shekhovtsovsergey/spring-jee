b6e6452698c596966e7f79f01a5227621ee12645
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2022-10-18 21:14:52 +0300

    Initial commit

---
+++ src/main/java/servlet/BasicServlet.java
@@ -0,0 +1,60 @@
+package servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+
+@WebServlet(name = "BasicServlet", urlPatterns = "/basic_servlet")
+public class BasicServlet implements Servlet {
+
+
+
+        private static Logger logger = LoggerFactory.getLogger(BasicServlet.class);
+        private transient ServletConfig config;
+
+
+        // Метод вызывается контейнером после того как был создан класс сервлета
+        @Override
+        public void init(ServletConfig config) throws ServletException { // Сохраняем полученную от сервера конфигурацию
+            this.config = config;
+        }
+
+
+
+        @Override
+        public ServletConfig getServletConfig() {
+            return config;
+        }
+
+
+        // Метод вызывается для каждого нового HTTP запроса к данному сервлету
+        @Override
+        public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+            logger.info("New request");
+
+            // получаем объект типа BufferedWriter и пишем в него ответ на пришедший запрос
+            res.getWriter().println("<h1>Servlet content</h1>");
+        }
+
+
+
+        @Override
+        public String getServletInfo() { return "BasicServlet";
+        }
+
+
+
+        // При завершении работы веб приложения, контейнер вызывает этот метод длявсех сервлетов из этого приложения
+        @Override
+        public void destroy() {
+            logger.info("Servlet {} destroyed", getServletInfo());
+        }
+
+
+
+}
+
